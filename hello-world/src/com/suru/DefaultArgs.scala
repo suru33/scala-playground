06b799dbd9b09a90dfa6e6cbf4a7bd175e510bcb
SuRu <su33.dev@gmail.com>
2020-03-08 19:17:53 +0200

    updated default args example

--- hello-world/src/com/suru/DefaultArgs.scala
+++ hello-world/src/com/suru/DefaultArgs.scala
@@ -1,11 +1,15 @@
 package com.suru
 
 object DefaultArgs extends App {
-    def repStr(s: String, acc: String, n: Int): String = {
-        if (n < 1) acc else repStr(s, s + acc, n - 1)
+
+    def repStr(s: String, n: Int): String = {
+        def helper(acc: String, t: Int): String =
+            if (t <= 1) acc else helper(s + acc, t - 1)
+
+        helper(s, n)
     }
 
-    println(repStr("Hello", "", 3))
+    println(repStr("Hello", 3))
     // acc doesn't need a initial value
 
     def repStrDef(s: String, n: Int, acc: String = ""): String = {
